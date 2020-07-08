1583b3660262f54f41380d73ea4c2527c6ef19fb
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-08 12:49:24 -0400

    created random dice roll and changed the dice images on refresh

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
 
-function randomNumber1(){
-  return Math.floor((Math.random() * 6) + 1);
-}
- console.log(randomNumber1())
+var randomDiceImage = "./images/dice" + randomNumber1 + ".png"
+
+var image1 = document.querySelectorAll("img")[0]
+var image2 = document.querySelectorAll("img")[1]
+
+image1.setAttribute("src", randomDiceImage)
+image2.setAttribute("src", randomDiceImage)
